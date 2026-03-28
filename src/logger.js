9a1895d621c0f9c9843084ebf96553ed958c88f3
eldar867 <eldar7012004@gmail.com>
2026-03-28 21:39:39 +0300

    Прогресс скачивания выводится в терминал

--- src/logger.js
+++ src/logger.js
@@ -1,1 +1,15 @@
-
+const logger = {
+  log: (url, success = true) => {
+    const symbol = success ? '✔' : '✘';
+    const color = success ? '\x1b[32m' : '\x1b[31m';
+    const reset = '\x1b[0m';
+    console.log(`${color}${symbol}${reset} ${url}`);
+  },
+  success: (filename) => {
+    console.log(`\nPage was downloaded as '${filename}'`);
+  },
+  error: (error) => {
+    console.error(`\nError: ${error.message}`);
+  }
+};
+export default logger;
