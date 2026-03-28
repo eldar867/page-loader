21aaa05df67619fc272281af7edb861a8e78ad7d
eldar867 <eldar7012004@gmail.com>
2026-03-28 21:41:01 +0300

    Публичный API модуля

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import downloadPage from './downloadPage.js';
+export { downloadPage };
 
+export const download = async (url, outputDir) => {
+  const filepath = await downloadPage(url, outputDir);
+  return { filepath, url };
+};
+
+export default { download, downloadPage };
